091f93163227e673ff7cde9d0b4704106627ff94
Tim Eifler <tim.eif@protonmail.com>
2022-03-06 14:47:26 +0100

    Add jest for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Home from './pages/Home.js';
+import styled from 'styled-components';
+
+//import { ErrorBoundary } from 'react-error-boundary';
+//import ErrorFallback from './components/ErrorFallback.js';
+
 import { useState } from 'react';
 
 function App() {
