b5035fc3f20524761f4292b6a4edb6218d50f4a3
Tim Eifler <tim.eif@protonmail.com>
2022-03-07 20:49:36 +0100

    Chang Header Layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import styled from 'styled-components';
 function Header() {
-  return <h1>CoinFox</h1>;
+  return <Headline>CoinFox</Headline>;
 }
 
 export default Header;
+
+const Headline = styled.h1`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #2f66f6;
+  height: 80px;
+  border-bottom: 1px solid black;
+  margin-bottom: 24px;
+`;
