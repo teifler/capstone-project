7be6fee0e1edb35af2ecfcc8ec23c90f2d85bea1
Tim Eifler <tim.eif@protonmail.com>
2022-03-07 19:16:27 +0100

    Add Loading state

--- src/App.js
+++ src/App.js
@@ -2,33 +2,37 @@ import React from 'react';
 import Home from './Home.js';
 //import styled from 'styled-components';
 import { useEffect, useState } from 'react';
-
 //import { ErrorBoundary } from 'react-error-boundary';
 //import ErrorFallback from './components/ErrorFallback.js';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [coins, setCoins] = useState([]);
-
+  const [currency, setCurrency] = useState('eur');
   useEffect(() => {
     const fetchData = async () => {
-      const currency = 'eur';
       const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
       try {
         const response = await fetch(url);
         const data = await response.json();
         setCoins(data);
+        setIsLoading(false);
       } catch (error) {
         console.error('ERROR:', error);
       }
     };
     fetchData();
   }, []); // Fetch API Data
+
+  if (isLoading) {
+    return <>isLoading...</>;
+  }
   return (
     <>
       {
         //<ErrorBoundary FallbackComponent={ErrorFallback}>
       }
-      <Home coins={coins} />
+      <Home coins={coins} currency={currency} />
     </>
   );
 }
