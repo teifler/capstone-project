94bdac3a852bd868259efd8256e99e3ab24784bb
Tim Eifler <tim.eif@protonmail.com>
2022-03-09 12:19:24 +0100

    Add searchbar with icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,38 +1,76 @@
 import styled from 'styled-components';
+import searchIcon from '../images/search.svg';
 
 function SearchBar({ handleChange }) {
   return (
-    <InputField
-      id="search-input"
-      name="search-input"
-      onChange={handleChange}
-      className="searchInput"
-      type="text"
-      pattern="[A-Za-z0-9_]{1,15}"
-      placeholder="Type to Search"
-    ></InputField>
+    <label htmlFor="search-input">
+      <SearchBarContainer>
+        <SearchInputContainer>
+          <SearchIcon>
+            <img alt="Search icon" src={searchIcon}></img>
+          </SearchIcon>
+          <InputField
+            aria-label="search-input"
+            id="search-input"
+            name="search-input"
+            onChange={handleChange}
+            className="searchInput"
+            type="text"
+            placeholder="Type to Search"
+          ></InputField>
+        </SearchInputContainer>
+      </SearchBarContainer>
+    </label>
   );
 }
 
 export default SearchBar;
 
+const SearchBarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 19rem;
+  height: 2.5rem;
+  border-radius: 6px;
+  overflow: hidden;
+  box-shadow: 0px 1px 5px 2px #6299ff;
+  margin-bottom: 10px;
+  background-color: #fff;
+`;
+
+//For SearchIcon or close Icon
+const SearchInputContainer = styled.div`
+  width: 100%;
+  height: 100%;
+
+  padding: 2px 15px;
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
 const InputField = styled.input`
-  width: 289px;
-  height: 40px;
-  margin-bottom: 20px;
-  /* Align Textbox Center */
-  ::-webkit-input-placeholder {
-    text-align: center;
+  width: 100%;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  border-radius: 6px;
+  height: 100%;
+  &:focus {
+    outline: none;
+    &::placeholder {
+      opacity: 0;
+    }
   }
-  :-moz-placeholder {
-    /* Firefox 18- */
-    text-align: center;
+  &::placeholder {
+    transition: 250ms ease-in-out;
   }
-  ::-moz-placeholder {
-    /* Firefox 19+ */
-    text-align: center;
-  }
-  :-ms-input-placeholder {
-    text-align: center;
-  }
+`;
+
+const SearchIcon = styled.span`
+  color: #a9abb1;
+  font-size: 1rem;
+  margin-right: 10px;
+  margin-top: 3px;
+  vertical-align: middle;
 `;
