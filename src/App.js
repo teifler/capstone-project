303e0a3503ef5762da925fff97a2a8cea3bf059a
Tim Eifler <tim.eif@protonmail.com>
2022-03-11 21:53:48 +0100

    Add React router and new page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import Home from './Home.js';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import spinner from './images/spinner.svg';
+import CoinPage from './pages/CoinPage.js';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   const [currency, setCurrency] = useState('eur');
   const [error, setError] = useState('');
+
+  const sample = {
+    id: 'bitcoin',
+    symbol: 'btc',
+    name: 'Bitcoin',
+    image:
+      'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 35245,
+    market_cap: 668970695709,
+    market_cap_rank: 1,
+    fully_diluted_valuation: 740302859784,
+    total_volume: 17926994182,
+    high_24h: 35977,
+    low_24h: 34808,
+    price_change_24h: -515.460254804602,
+    price_change_percentage_24h: -1.44141,
+    market_cap_change_24h: -13264263108.407715,
+    market_cap_change_percentage_24h: -1.94424,
+    circulating_supply: 18976537,
+    total_supply: 21000000,
+    max_supply: 21000000,
+    ath: 59717,
+    ath_change_percentage: -40.96719,
+    ath_date: '2021-11-10T14:24:11.849Z',
+    atl: 51.3,
+    atl_change_percentage: 68620.50031,
+    atl_date: '2013-07-05T00:00:00.000Z',
+    roi: null,
+    last_updated: '2022-03-07T09:22:37.003Z',
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,17 +60,32 @@ function App() {
   if (isLoading) {
     return <SpinnerLogo src={spinner} height="80" width="80"></SpinnerLogo>;
   }
+
+  console.log(coins);
   return (
-    <>
-      {!error ? (
+    <Routes>
+      <Route
+        path="/"
+        element={<HomePage coins={coins} currency={currency} />}
+      />
+      {coins.map(coin => (
+        <Route
+          path={`${coin.id}`}
+          element={
+            <CoinPage title={coin.name} coin={coin} currency={currency} />
+          }
+        />
+      ))}
+
+      {/*{!error ? (
         <Home coins={coins} currency={currency} />
       ) : (
         <ErrorMessage>
           We had issues fetching the coins for you. Please reload the page to
           try it again!
         </ErrorMessage>
-      )}
-    </>
+      )}*/}
+    </Routes>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,119 @@
+import Header from '../components/Header.js';
+import { useState } from 'react';
+import CoinCard from '../components/CoinCard.js';
+import styled from 'styled-components';
+import SearchBar from '../components/SearchBar.js';
+
+function HomePage({ coins, currency }) {
+  const [search, setSearch] = useState('');
+  const [searchError, setSearchError] = useState(false);
+
+  const filterdCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleChange = e => {
+    setSearch(e.target.value.trim());
+
+    if (e.target.value.trim() !== '') {
+      if (!e.target.value.trim().match(/^[a-zA-Z0-9_.]+$/)) {
+        setSearchError(true);
+      } else {
+        setSearchError(false);
+      }
+    } else {
+      setSearchError(false);
+    }
+  };
+  return (
+    <>
+      <Header title="Crypto Cloud" />
+      <CoinSearch>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <SearchBar handleChange={handleChange} />
+          {searchError ? (
+            <SearchErrorMessage>
+              Please don't use special characters
+            </SearchErrorMessage>
+          ) : (
+            ''
+          )}
+        </form>
+      </CoinSearch>
+      <TableHeading>
+        <p>#</p>
+        <p>Name</p>
+        <StackedHeading>
+          <p>Price</p>
+          <p>% Change 24H</p>
+        </StackedHeading>
+      </TableHeading>
+      <CoinList role="list">
+        {filterdCoins.map(coin => {
+          return (
+            <CoinCard
+              currency={currency}
+              rank={coin.market_cap_rank}
+              id={coin.id}
+              key={coin.id}
+              name={coin.name}
+              symbol={coin.symbol}
+              image={coin.image}
+              price={coin.current_price}
+              price_change_percentage_24h={coin.price_change_percentage_24h}
+            />
+          );
+        })}
+      </CoinList>
+      {filterdCoins.length === 0 ? (
+        <CoinNotFound>Searched coin is not in the database</CoinNotFound>
+      ) : (
+        ''
+      )}
+    </>
+  );
+}
+
+export default HomePage;
+
+const CoinList = styled.ul``;
+
+const CoinSearch = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TableHeading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  margin: 0 auto;
+  padding: 5px 0 5px;
+  width: 90%;
+  margin-bottom: 24px;
+`;
+
+const StackedHeading = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+const SearchErrorMessage = styled.p`
+  color: red;
+  text-align: center;
+  padding-bottom: 5px;
+`;
+
+const CoinNotFound = styled.p`
+  color: black;
+  text-align: center;
+`;
