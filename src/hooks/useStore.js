e11527e43cc8c9dd55a8bafddceb42585fe4ce27
Tim Eifler <tim.eif@protonmail.com>
2022-03-22 19:00:11 +0100

    Add persist middleware to store data in localstorage

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,6 +1,7 @@
 import create from 'zustand';
 import axios from 'axios';
 import { produce } from 'immer';
+import { persist } from 'zustand/middleware';
 
 function initialize() {
   return {
@@ -10,92 +11,97 @@ function initialize() {
   };
 }
 
-const useStore = create((set, get) => {
-  return {
-    currency: 'eur',
-    days: 1,
-    meta: {
-      coins: {},
-    },
-    setMeta(key, id, partial) {
-      set(
-        produce(draft => {
-          //draft.meta[key][id] = draft.meta[key][id] ?? partial;
-          //draft.meta[key][id] = draft.meta[key][id] ?? {};
-          if (draft.meta[key][id]) {
-            Object.entries(partial).forEach(entry => {
-              const [key_, value_] = entry;
-              draft.meta[key][id][key_] = value_;
-            });
-          } else {
-            draft.meta[key][id] = partial;
-          }
-        })
-      );
-    },
-    search: {
-      input: '',
-      error: null,
-    },
-    coins: initialize(),
-    isVisible: false,
-    setVisible(visibility) {
-      set({
-        isVisible: visibility,
-      });
-    },
-    chartHistory: initialize(),
-    setSearchInput(userInput, errorState) {
-      set({
-        search: {
-          input: userInput,
-          error: errorState,
+const useStore = create(
+  persist(
+    (set, get) => {
+      return {
+        currency: 'eur',
+        days: 1,
+        meta: {
+          coins: {},
         },
-      });
-    },
-    setDays(selectedTimeframe) {
-      set({
-        days: selectedTimeframe,
-      });
-    },
-    setCurrency(newCurrency) {
-      set({
-        currency: newCurrency,
-      });
-    },
-    async getData(url, key) {
-      const { data: previousData } = get()[key];
-      set({
-        [key]: {
-          data: previousData,
-          loading: true,
+        setMeta(key, id, partial) {
+          set(
+            produce(draft => {
+              //draft.meta[key][id] = draft.meta[key][id] ?? partial;
+              //draft.meta[key][id] = draft.meta[key][id] ?? {};
+              if (draft.meta[key][id]) {
+                Object.entries(partial).forEach(entry => {
+                  const [key_, value_] = entry;
+                  draft.meta[key][id][key_] = value_;
+                });
+              } else {
+                draft.meta[key][id] = partial;
+              }
+            })
+          );
+        },
+        search: {
+          input: '',
           error: null,
         },
-      });
-      try {
-        const { data } = await axios.get(url);
-        setTimeout(
-          () =>
+        coins: initialize(),
+        isVisible: false,
+        setVisible(visibility) {
+          set({
+            isVisible: visibility,
+          });
+        },
+        chartHistory: initialize(),
+        setSearchInput(userInput, errorState) {
+          set({
+            search: {
+              input: userInput,
+              error: errorState,
+            },
+          });
+        },
+        setDays(selectedTimeframe) {
+          set({
+            days: selectedTimeframe,
+          });
+        },
+        setCurrency(newCurrency) {
+          set({
+            currency: newCurrency,
+          });
+        },
+        async getData(url, key) {
+          const { data: previousData } = get()[key];
+          set({
+            [key]: {
+              data: previousData,
+              loading: true,
+              error: null,
+            },
+          });
+          try {
+            const { data } = await axios.get(url);
+            setTimeout(
+              () =>
+                set({
+                  [key]: {
+                    data,
+                    loading: false,
+                    error: null,
+                  },
+                }),
+              2000
+            );
+          } catch (error) {
             set({
               [key]: {
-                data,
+                data: previousData,
                 loading: false,
-                error: null,
+                error,
               },
-            }),
-          2000
-        );
-      } catch (error) {
-        set({
-          [key]: {
-            data: previousData,
-            loading: false,
-            error,
-          },
-        });
-      }
+            });
+          }
+        },
+      };
     },
-  };
-});
+    { name: 'Cryptocloud' }
+  )
+);
 
 export default useStore;
