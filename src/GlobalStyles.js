8424bc29edee4cf66508e6780429d28f0792c2e8
Tim Eifler <tim.eif@protonmail.com>
2022-03-27 22:29:35 +0200

    Add styling to header and add global color variables

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -7,8 +7,14 @@ export default createGlobalStyle`
     margin: 0;
   }
 
+  :root{
+    --bg-color-main: #F7F8FD;
+    --bg-color-gradientblue: linear-gradient(90deg, #00CBFF -0.55%, #6721FF 101.05%), #160042;
+  }
+
   body {
     font-family: 'Roboto', sans-serif;
+    background-color: --bg-color-main;
   }
 
   a {
