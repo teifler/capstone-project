13803b890082f33718d291d39fd5fd97d8727857
Tim Eifler <tim.eif@protonmail.com>
2022-03-28 12:06:34 +0200

    Apply new layout and design to page

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -13,12 +13,25 @@ import arrowDown from '../images/arrow-down.svg';
 import spinner from '../images/spinner.svg';
 
 function CoinPage({ coin, title, currency }) {
+  const singleCoin = useStore(state => state.singleCoin);
   const days = useStore(state => state.days);
   const setDays = useStore(state => state.setDays);
   const meta = useStore(state => state.meta);
   const chartHistory = useStore(state => state.chartHistory);
 
   const coinId = coin.id;
+
+  useEffect(() => {
+    useStore
+      .getState()
+      .getData(
+        `https://api.coingecko.com/api/v3/coins/${coinId}`,
+        'singleCoin'
+      );
+  }, [coinId, currency, days]);
+
+  console.log('Single Coin', singleCoin.data);
+
   useEffect(() => {
     useStore
       .getState()
@@ -46,32 +59,130 @@ function CoinPage({ coin, title, currency }) {
         </ErrorMessage>
       ) : (
         <CardWrapper>
-          <GoBack to="/">
-            <img alt="arrow-left" src={arrowLeft} hight="35" width="35"></img>{' '}
-          </GoBack>
-          <TrackButton onClick={() => toggleBookmark(coin.id)}>
-            {meta.coins[coin.id]?.bookmarked ? (
-              <img
-                src={starFilled}
-                alt="Remove coin of your tracking list"
-                height="30"
-                width="30"
-              ></img>
-            ) : (
-              <img
-                src={star}
-                alt="Add coin to your tracking list"
-                height="30"
-                width="30"
-              ></img>
-            )}
-          </TrackButton>
-          <CoinImages>
-            <img alt={coin.id} src={coin.image} height="80"></img>
-          </CoinImages>
-          <CoinName>
-            <h2>{coin.name}</h2>
-          </CoinName>
+          <InfoContainer>
+            <GoBack to="/">
+              <img alt="arrow-left" src={arrowLeft} hight="26" width="26"></img>{' '}
+            </GoBack>
+            <h4>
+              {coin.name} <span>({coin.symbol.toUpperCase()})</span>
+            </h4>
+            <TrackButton onClick={() => toggleBookmark(coin.id)}>
+              {meta.coins[coin.id]?.bookmarked ? (
+                <img
+                  src={starFilled}
+                  alt="Remove coin of your tracking list"
+                  height="30"
+                  width="30"
+                ></img>
+              ) : (
+                <img
+                  src={star}
+                  alt="Add coin to your tracking list"
+                  height="30"
+                  width="30"
+                ></img>
+              )}
+            </TrackButton>
+          </InfoContainer>
+          <SideBarImageContainer>
+            <PriceContainer>
+              <p>{coin.name} Price</p>
+              <h4>
+                {currency === 'eur'
+                  ? `${coin.current_price
+                      .toFixed(2)
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}€`
+                  : `$${coin.current_price
+                      .toFixed(2)
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`}
+              </h4>
+              <p>
+                Last 24hrs:
+                {coin.price_change_24h >= 0 ? (
+                  <PriceUp>
+                    {' +'}
+                    {currency === 'eur'
+                      ? `${coin.price_change_24h
+                          .toFixed(2)
+                          .toString()
+                          .replace(
+                            /\B(?=(\d{3})+(?!\d))/g,
+                            ','
+                          )}€ (+${coin.price_change_percentage_24h.toFixed(
+                          2
+                        )}%)`
+                      : `$${coin.price_change_24h
+                          .toFixed(2)
+                          .toString()
+                          .replace(
+                            /\B(?=(\d{3})+(?!\d))/g,
+                            ','
+                          )} (+${coin.price_change_percentage_24h.toFixed(
+                          2
+                        )}%)`}
+                  </PriceUp>
+                ) : (
+                  <PriceDown>
+                    {' '}
+                    {currency === 'eur'
+                      ? `${coin.price_change_24h
+                          .toFixed(2)
+                          .toString()
+                          .replace(
+                            /\B(?=(\d{3})+(?!\d))/g,
+                            ','
+                          )}€ (-${coin.price_change_percentage_24h.toFixed(
+                          2
+                        )}%)`
+                      : `${coin.price_change_24h
+                          .toFixed(2)
+                          .toString()
+                          .replace(
+                            /\B(?=(\d{3})+(?!\d))/g,
+                            ','
+                          )}$ (${coin.price_change_percentage_24h.toFixed(
+                          2
+                        )}%)`}
+                  </PriceDown>
+                )}
+              </p>
+            </PriceContainer>
+            <CicleContainer>
+              <CircleImage>
+                <CoinImages>
+                  <img alt={coin.id} src={coin.image} height="32"></img>
+                </CoinImages>
+              </CircleImage>
+            </CicleContainer>
+          </SideBarImageContainer>
+          <MarketInformationContainer>
+            <div>
+              <h4>Market Cap</h4>
+              <h3>${coin.market_cap}</h3>
+            </div>
+            <div>
+              <h4>Volume (24h)</h4>
+              <h3></h3>
+            </div>
+            <div>
+              <h4>Popularity</h4>
+              <h3>#${coin.market_cap_rank}</h3>
+            </div>
+            <div>
+              <h4>Low (1 week)</h4>
+              <h3></h3>
+            </div>
+            <div>
+              <h4>High (1 week)</h4>
+              <h3></h3>
+            </div>
+            <div>
+              <h4>All time high</h4>
+              <h3></h3>
+            </div>
+          </MarketInformationContainer>
           <InformationWrapper>
             <h4>Market Information</h4>
             <StyledList role="list">
@@ -176,18 +287,33 @@ const CardWrapper = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  min-width: 340px;
   padding: 2rem;
-  position: relative;
   margin-bottom: 65px;
+  width: 90%;
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  width: 90%;
+  justify-content: space-between;
+
+  span {
+    color: var(--font-color-light);
+    font-size: 0.75;
+  }
+  margin-bottom: 30px;
+`;
+
+const PriceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 90%;
 `;
 
 const StyledList = styled.ul``;
 
-const GoBack = styled(NavLink)`
-  align-self: flex-start;
-`;
+const GoBack = styled(NavLink)``;
 
 const CoinName = styled.div`
   width: 90%;
@@ -223,19 +349,43 @@ const InformationWrapper = styled.div`
 `;
 
 const TrackButton = styled.button`
-  position: absolute;
   top: 32px;
   right: 15px;
   border-style: none;
   background-color: transparent;
 `;
 
-const CoinImages = styled.div`
+const CicleContainer = styled.div`
   display: flex;
   justify-content: center;
-  width: 90%;
-  margin-bottom: 15px;
+  align-items: center;
+  background: rgba(182, 185, 206, 0.09) center;
+  border-radius: 36px;
+  height: 72px;
+  width: 72px;
+  background-position: center;
+`;
+
+const CircleImage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(182, 185, 206, 0.16) center;
+  background-position: center;
+  border-radius: 25px;
+  height: 50px;
+  width: 50px;
+`;
+
+const CoinImages = styled.div`
   margin-right: 0;
+  padding-top: 2px;
+`;
+
+const SideBarImageContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
 `;
 
 const SelectTimeFrame = styled.select`
